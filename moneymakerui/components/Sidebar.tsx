74bbba18c71a96f87856ac4b3416f56326a8c78b
Tobias-Bla <tobias-blaschke@web.de>
2025-03-12 00:11:37 +0100

    dashboards created

--- moneymakerui/components/Sidebar.tsx
+++ moneymakerui/components/Sidebar.tsx
@@ -3,17 +3,17 @@
 import { usePathname } from "next/navigation";
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { FaChartPie, FaUserAlt, FaSignOutAlt, FaBriefcase } from "react-icons/fa";
+import { FaChartPie, FaUserAlt, FaBriefcase } from "react-icons/fa";
 
 const Sidebar = () => {
   const pathname = usePathname();
   const { data: session } = useSession(); // ✅ Now correctly inside Sidebar
   const router = useRouter();
 
-  async function handleLogout() {
-    await signOut({ redirect: false });
-    router.push("/login");
-  }
+  //async function handleLogout() {
+   // await signOut({ redirect: false });
+   // router.push("/login");
+  //}
 
   // ✅ Hide sidebar on login and signup
   if (["/login", "/signup"].includes(pathname)) return null;
